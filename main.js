8afd45ffd0d5ae05c8df4ec4072507a42fe72320
IGROMY <igromy99@gmail.com>
2023-04-22 22:38:37 +0300

    Start JS

--- main.js
+++ main.js
@@ -1,14 +1,56 @@
 import './styles/style.scss'
 
-// import { setupCounter } from './counter.js'
-//
-// document.querySelector('#testContainer').innerHTML = `
-//   <div>
-//     <h1>Click it please!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//   </div>
-// `
-//
-// setupCounter(document.querySelector('#counter'))
+//const number="print";
+
+//alert(number);
+//const car={ };//объект
+//const cars=[ car ]; //массив
+//console.log(cars);
+//const car={
+ // name:"WV"
+//};
+//console.log(car);
+// const user={
+//   name:"IG",
+//   email:"mix"
+// };
+//NULL
+//Symbols
+//bigint
+//const x=5;
+//const y=true;
+//console.log(y+x);
+//console.log(5%2);//остаток от деления
+//console.log(5**2);//возведение в степень
+//console.log(+"34");//унарный плюс
+//const z=3;
+//const c="3";
+//console.log(z==c,"сравнение равенсва");//нестрогое равенство
+//console.log(z===c,"сравнение равенсва");//строгое равенство
+//let n=3;
+//n=n+5;
+//n+=5;
+
+const x="";
+const y=1;
+const z=0;
+const q=true;
+const w=false;
+const n=null;
+const u=undefined;
+console.log(x+y+z);
+console.log(x-y+z);
+//console.log(q+w);
+console.log(true+false);
+console.log(6/"3");
+console.log("2"*"3");
+console.log(4+5+"px");
+console.log("$"+4+5);
+console.log("4"-2);
+console.log("4px"-2);
+console.log("-9"+5);
+console.log("-9"-5);
+console.log(n+1);
+console.log(u+1);
+console.log("\t\n"-2);
+
